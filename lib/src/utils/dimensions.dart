e4f633d7af43da475886df8d25287aeeb5e5c176
Ricardo Mejias Travieso <63120247+Ricardo-MT@users.noreply.github.com>
2023-05-30 14:00:07 +0200

    License and configuration before publishing

--- lib/src/utils/dimensions.dart
+++ lib/src/utils/dimensions.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 
 /// Spacing and general dimensions
 class WidAppDimensions {
+  /// Default size for borders
   static double borderWidth = 1.6;
+
+  /// Default radius for controllers
   static double borderRadiusControllers = 12;
+
+  /// Default height for controllers
   static double controllerHeight = 48;
+
+  /// Default spacing gap for widgets
   static double innerGap = 18;
+
+  /// Default spacing gap for cards
   static double cardSmallSpacing = 10;
+
+  /// Default large spacing gap for cards
   static double cardLargeSpacing = cardSmallSpacing * 2.2;
-  static double defaultCardWidth = 520;
-  static double defaultCardHeight = 420;
-  static double labelFontSize = 12;
-  static double cardTitleFontSize = labelFontSize + 3;
+
+  /// Default page inset
   static double pageInsetGap = 30;
+
+  /// Default animation duration
   static int animationDuration = 200;
+
+  /// Custom app header height
   static double customToolbarHeight = kToolbarHeight * 1.4;
 }
